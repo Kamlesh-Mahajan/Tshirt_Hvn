9bf390fceb65f3eca42805b41a3c036dd846a228
Kamlesh1222 <kamleshmahajan545@gmail.com>
2024-02-20 19:32:11 +0530

    error handlers
    error handlers  are created and product crud operation completed

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+const errorMiddleware = require("./middleware/error")
 app.use(express.json())
 
 //* Route Imports 
@@ -8,6 +9,7 @@ const product = require("./routes/productRoute");
 
 app.use("/api/v1",product);
 
+//*Middleware For Errors
+app.use(errorMiddleware);
 
-
-module.exports = app
+module.exports = app;
--- backend/config/database.js
+++ backend/config/database.js
@@ -5,11 +5,9 @@ const connectDatabase = () => {
     .connect(process.env.DB_URI )
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}
+    });
+   
+};
 
 module.exports = connectDatabase;
 
