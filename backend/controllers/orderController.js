f480f212f5b8414da8cbf460c57dbdc31d6e4f74
21bca158 <21bca158@vtcbb.edu.in>
2024-03-07 20:56:34 +0530

    90% backend
    complte

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -61,3 +61,73 @@ exports.myOrders = catchAsyncErrors(async (req, res, next) =>{
     });
   });
   
+// get all  Order --admin
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) =>{
+  const orders = await Order.find();
+
+    let totalAmount=0;
+    orders.forEach((order) => {
+        totalAmount += order.totalPrice;
+   
+    });
+  
+  res.status(200).json({
+    success:true,
+    totalAmount,
+    orders,
+  });
+});
+
+
+// update order status --admin
+exports.updateOrder = catchAsyncErrors(async (req, res, next) =>{
+  const order = await Order.findById(req.params.id);
+
+  if(!order){
+    return next(new ErrorHandler("Order not found with this id",404));
+  }
+
+    if (order.orderStatus === "Delivered"){
+      return next(new ErrorHandler("You have already deliverd this order",400));
+    }
+   
+
+    order.orderItems.forEach(async (o)=>{
+      await updateStock(o.Product.o.quantity);
+    });
+
+    order.orderStatus = req.body.status;
+    if (req.body.status === "Delivered"){
+      order.deliveredAt =  Date.now();
+    }
+
+    await order.save({ validateBeforeSave: false});
+  res.status(200).json({
+    success:true,
+   
+  });
+});
+
+async function updateStock(id, quantity){
+  const product =await Product.findById(id);
+
+  product.stock -=quantity;
+  await product.save({ validateBeforeSave: false})
+}
+
+// delete  Order --admin
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) =>{
+  const order = await Order.findById(req.params.id);
+
+  if(!order){
+    return next(new ErrorHandler("Order not found with this id",404));
+  }
+
+  await order.remove();
+    
+  res.status(200).json({
+    success:true,
+   
+  });
+});
+
